89969f2b36fa8f553f033e24ce22ba0492e4b7bd
AlexanderSYN <000777000casa@gmail.com>
2026-03-03 01:58:58 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# программа для теста
+> [!TIP]
+> при запуске программа выглядит так
+<img src="img/screenshot.png" />
+
+и выводит:
+2026-03-03 01:43:25.872  7759-7759  [DEBUG_RANDOM_NUM]      com.example.myapplication            D  Число: 6
---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        setRequestedOrintation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+        var r: Int = Random.nextInt(1, 7)
+        Log.d("[DEBUG_RANDOM_NUM] ", "Число: $r")
+    }
+
+    private fun setRequestedOrintation(screenOrientationLandscape: Int) {
+        super.requestedOrientation = screenOrientationLandscape
+    }
+
+
+}
